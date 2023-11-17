b9240f2b42a4a9cdceae3b89fcf95335c1609ed3
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-11-17 18:13:33 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require 'functions.php';
+
+dd($_SERVER);
+// dd($_SERVER['REQUEST_URI']);
+// dd(parse_url($_SERVER['REQUEST_URI'])['path']);
+// dd(str_ends_with($_SERVER['REQUEST_URI'], '/'));
+// dd(str_ends_with($_SERVER['REQUEST_URI'], '/fred'));
+
