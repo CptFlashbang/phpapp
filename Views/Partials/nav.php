47d0706dceb0613d59c47256a9210c273533a02b
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2023-11-17 21:29:08 +0000

    Contact page added
    Contact page added, routing and nav amended

--- Views/Partials/nav.php
+++ Views/Partials/nav.php
@@ -6,5 +6,8 @@
     <li class="grow p-0 w-1/5 border border-black border-solid box-border">
       <a href="/phpapp/about" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/phpapp/about') ? 'aria-current="page"': '' ?>>About</a>
     </li>
+    <li class="grow p-0 w-1/5 border border-black border-solid box-border">
+      <a href="/phpapp/contact" class="leading-[3] min-h-[45px] block text-slate-50 text-center" <?= urlIs('/phpapp/contact') ? 'aria-current="page"': '' ?>>Contact</a>
+    </li>
   </ul>
 </nav>
--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
 <?php
+
 return [
     '/phpapp/' => 'controllers/index.php',
-    '/phpapp/about' => 'controllers/about.php'
+    '/phpapp/about' => 'controllers/about.php',
+    '/phpapp/contact' => 'controllers/contact.php'
 
 ];
