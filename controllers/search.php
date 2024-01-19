2cfa54cb2bb2964791377d11f1364ed562e11b96
Callum Yates <47091219+CptFlashbang@users.noreply.github.com>
2024-01-19 14:35:35 +0000

    Update search.php

--- controllers/search.php
+++ controllers/search.php
@@ -1,9 +1,20 @@
 <?php
-$heading = "Search";
-$config = require('config.php');
-$db = new Database($config['database']);
-$query = "select * from books";
-$books = $db->query($query)->get();
+$heading = "Search Books";
+$books = [];
 $records="";
 $return="";
+if(isset($_GET['title']) && !empty($_GET['title'])){
+    $return="search?title=".htmlspecialchars($_GET['title']);
+    $config = require('config.php');
+    $db = new Database($config['database']);
+    $query = "select * from books where title like :title";
+    $filter = "%".htmlspecialchars($_GET['title'])."%";
+    $books = $db->query($query, ['title' => $filter])->get();
+    if (count($books) == 0) {
+        $records = "You have found no matches, please try again";
+    } else
+    {
+        $records = "You have found ". count($books) . " records from your search";
+    }
+}
 require "views/search.view.php";
